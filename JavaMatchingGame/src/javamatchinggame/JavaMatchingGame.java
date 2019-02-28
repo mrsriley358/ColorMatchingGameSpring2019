31959ee02a33c2443b46ee1a856eecdc76c0d092
cb0806151 <43824792+cb0806151@users.noreply.github.com>
2019-02-28 13:13:02 -0600

    adding in methods for clicking on panels

--- JavaMatchingGame/src/javamatchinggame/JavaMatchingGame.java
+++ JavaMatchingGame/src/javamatchinggame/JavaMatchingGame.java
@@ -5,9 +5,14 @@
  */
 package javamatchinggame;
 
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -15,14 +20,50 @@ import javafx.stage.Stage;
  * @author cb0806151
  */
 public class JavaMatchingGame extends Application {
+    Boolean paused = false;
+    Color lastPanelClicked;
+    ArrayList<Rectangle> isSelected;
+    Integer panelsSelected = 0;
     @Override
     public void start(Stage stage) {
-        
-        
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
         Button reset = new Button("Reset");
-        
-        pane.add(reset, 1, 4);
+        pane.add(reset, 2, 5);
     }
+    
+    public void pauseAndResetPanels() {
+        paused = true;
+        Thread.sleep(1000);
+        for (int i = 0; i<isSelected.size()-1; i++) {
+            isSelected.get(i).setFill(Color.GRAY);
+        }
+        isSelected.clear();
+        panelsSelected = 0;
+        paused = false;
+    }   
+    
+    public void clickOnPanel(Rectangle panel, Color theColor) {
+        panel.setOnMouseClicked(e -> {
+            if (paused == false) {
+                panel.setFill(theColor);
+                if (lastPanelClicked != theColor && lastPanelClicked != null) {
+                    pauseAndResetPanels();
+                } else {
+                    lastPanelClicked = theColor;
+                    panelsSelected += 1;
+                    isSelected.add(panel);
+                }
+                
+                if (panelsSelected == 3) {
+                    panelsSelected = 0;
+                    lastPanelClicked = null;
+                    isSelected.clear();
+                }
+            }
+        });
+    }
+    
     /**
      * @param args the command line arguments
      */
